e35d56b4520c1e0b9e72e0d9ea7fb21383833557
Michael Chang <michaelchang@nat-oitwireless-inside-vapornet100-c-18653.Princeton.EDU>
2016-02-20 16:10:20 -0500

    stacking features

--- tools/learning_script.py
+++ tools/learning_script.py
@@ -11,20 +11,37 @@ from sklearn.metrics import zero_one_loss
 import csv
 import numpy as np
 
-exempler = []
+feature_list = ['FV_MFCC.csv', 'FV_chroma.csv', 'FV_brightness.csv', 'FV_zerocross.csv']
+fisher_directory = './../fisher_vectors/'
+feature_data = []
+SONG_SET_SIZE = 1000
 
-#Parse CSV file, wher each row corresponds to a particular exempler
-with open('FV_MFCC.csv', 'rb') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        float_row = np.array([float(i) for i in row])
-        exempler.append(float_row)
+for x in range(0,SONG_SET_SIZE):
+    feature_data.append([])
 
-exempler = np.array(exempler)
+#Parse CSV file, where each row corresponds to a particular exempler
+#Concatenate all the features in the same list
+for feature_file in feature_list:
+    with open(fisher_directory + feature_file, 'rb') as f:
+        reader = csv.reader(f)
+        temp_feature_data = []
+        for row in reader:
+            float_row = [float(i) for i in row]
+            temp_feature_data.append(float_row)
+        
+        count = 0
+        for song_temp_feature in temp_feature_data:
+            feature_data[count] += song_temp_feature
+            print count
+            count += 1
+
+print feature_data[0]
+
+feature_data = np.array(feature_data)
 
 maxLearners = 100
 maxDepth = 5
-N = len(exempler)
+N = len(feature_data)
 labels = np.array([N,])
 
 #Parse the labels file
@@ -34,8 +51,8 @@ with open('labels.csv', 'rb') as f:
         labels = np.array([int(i) for i in label])
 
 TEidx = np.array(random.sample(range(0,N), N/10))
-X_TE = exempler[TEidx, :]
-X_TR = exempler[[i for i in range(0,N) if i not in TEidx],:]
+X_TE = feature_data[TEidx, :]
+X_TR = feature_data[[i for i in range(0,N) if i not in TEidx],:]
 Y_TE = labels[TEidx]
 Y_TR = labels[[i for i in range(0,N) if i not in TEidx]]
 
