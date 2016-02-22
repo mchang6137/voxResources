dbccfac765526de0098274e6690718c99d245d80
Steven Lyubomirsky <slyubomirsky@gmail.com>
2016-02-22 00:01:40 -0500

    Added function for grid searching parameters for RF and SVC

--- tools/learning_script.py
+++ tools/learning_script.py
@@ -8,17 +8,31 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
 from sklearn.metrics import zero_one_loss
 import sklearn.cross_validation as cv
 
 import csv
 import numpy as np
 
+# does grid search with the given learner, data, and labels on the
+# specified param grid, prints best params, returns the best learner 
+# produced by the search
+def tuneLearner(name, learner, param_grid, data, labels):
+    search = GridSearchCV(estimator = learner, param_grid = param_grid)
+    search.fit(data, labels)
+    print "-- %s Grid Search Parameters --" % name
+    for param in search.best_params_:
+        print "%s: %s" % (param, str(search.best_params_[param]))
+    print "--\n"
+    return search.best_estimator_
+
 def evaluateLearner(name, learner, data, labels, splits):
     scores = cv.cross_val_score(learner, data, labels, cv = splits)
     print "-- %s Cross Validation Score --" % name
     print "Mean score: %f" % scores.mean()
     print "Standard deviation: %f" % scores.std()
+    print "--\n"
 
 feature_list = ['FV_MFCC.csv', 'FV_chroma.csv', 'FV_brightness.csv', 'FV_zerocross.csv']
 fisher_directory = './../fisher_vectors/'
@@ -56,19 +70,33 @@ with open(fisher_directory + 'labels.csv', 'rb') as f:
     for label in reader:
         labels = np.array([int(i) for i in label])
 
-rf = RandomForestClassifier(n_estimators = maxLearners, max_depth = maxDepth, warm_start = False)
+# tune random forest using grid search and evaluate
+rf = RandomForestClassifier(warm_start = False)
+rf_param_grid = [
+    {'n_estimators': [10, 25, 50, 75, 100], 'max_depth': [4, 5, 6, 7]}
+]
+final_rf = tuneLearner("Random Forest", rf, rf_param_grid, feature_data, labels)
+evaluateLearner("Random Forest", rf, feature_data, labels, 10)
 
 # tune AdaBoost using grid search
-boost = AdaBoostClassifier()
-param_grid = [
-    {'n_estimators': [25, 50, 75, 100], 'learning_rate': [0.1, 0.125, 0.25, 0.5, 0.75, 1]}
+#boost = AdaBoostClassifier()
+#boost_param_grid = [
+    #{'n_estimators': [25, 50, 75, 100], 'learning_rate': [0.1, 0.125, 0.25, 0.5, 0.75, 1]}
+#]
+#final_boost = tuneLearner("Adaboost", boost, boost_param_grid, feature_data, labels)
+#evaluateLearner("Adaboost", final_Boost, feature_data, labels, 10)
+
+# tune SVM using grid search and evaluate
+svm = SVC()
+svm_param_grid = [
+    {'C': [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75], 'kernel': ['linear', 'rbf']}
 ]
-search = GridSearchCV(estimator = boost, param_grid = param_grid)
-search.fit(feature_data, labels)
+final_svm = tuneLearner("SVM", svm, svm_param_grid, feature_data, labels)
 
-finalBoost = search.best_estimator_
-boostParam = search.best_params_
+svm_search = GridSearchCV(estimator = svm, param_grid = svm_param_grid)
+svm_search.fit(feature_data, labels)
 
-evaluateLearner("Random Forest", rf, feature_data, labels, 10)
-evaluateLearner(("Adaboost with %d Learners, %f Rate" % (boostParam['n_estimators'], boostParam['learning_rate'])),
-    finalBoost, feature_data, labels, 10)
+final_svm = svm_search.best_estimator_
+svm_params = svm_search.best_params_
+
+evaluateLearner("SVM", final_svm, feature_data, labels, 10)
